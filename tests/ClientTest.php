5e73a46a86b6271c60f3e665ba20e08382143422
Ben Norcombe <b.norcombe@gmail.com>
2015-10-15 11:35:15 +0100

    Setup Unit test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -6,13 +6,11 @@ use Maenbn\GitlabCiBuildStatus\Client;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
-
-    public function testStatusReturnsString(){
-
+    public function testStatusReturnsString()
+    {
         $client = new Client('https://gitlab.com/ci', 1, '0e6528a230ce89d8a2939080867a22');
         $status = $client->getStatus();
 
         $this->assertTrue(is_string($status));
     }
-
 }
