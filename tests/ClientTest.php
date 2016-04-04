1bd8ce36f5cbbcd4cb2d9223f46a6e8847c5fcbc
Ben Norcombe <b.norcombe@gmail.com>
2016-04-04 14:14:59 +0100

    Add test to check failed status return failed

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -13,6 +13,13 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(is_string($status));
     }
 
+    public function testFailedStatusReturnsString()
+    {
+        $client = new Client('https://gitlab.com/api/v3', 1031075, getenv('GITLAB_PRIVATE_KEY'));
+        $status = $client->getStatus('failing');
+        $this->assertTrue(is_string($status));
+    }
+
     /**
      * @expectedException \Exception
      * @expectedExceptionMessageRegExp #Error:.*#
